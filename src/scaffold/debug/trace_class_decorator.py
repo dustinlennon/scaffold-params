8876862649ea50ef156f2315f5419698ff993d6d
Dustin Lennon <dustin.lennon@gmail.com>
2025-03-31 14:38:49 -0700

    Fixed an edge case bug where TraceClassDecorator was generating RecursionError on some instance methods with custom __repr__ calls.

--- src/scaffold/debug/trace_class_decorator.py
+++ src/scaffold/debug/trace_class_decorator.py
@@ -3,7 +3,7 @@ import functools
 import textwrap
 
 from scaffold.debug import _printf_debug, ansi_color_codes as color
-
+ 
 class TraceClassDecorator(object):
   _indent = 0
 
@@ -12,17 +12,45 @@ class TraceClassDecorator(object):
     self._exclude = exclude
     self._mro     = mro
 
+  def _omit_class(self, cls):
+    if cls.__name__.startswith("_"):
+      return True
+    elif cls is object:
+      return True
+    else:
+      return False
+
   def _get_class_set(self, cls):
     if self._mro:
       classes = cls.__mro__
     else:
       classes = [ cls ]
+
+    classes = [ K for K in classes if not self._omit_class(K) ]
+
     return classes
 
   @classmethod
-  def _debug_call(cls, qual_name, *args, **kw):
-    args_s = ", ".join([repr(a) for a in args])
-    kw_s   = ", ".join([f"{k}={repr(v)}" for k,v in kw.items()])
+  def _debug_call(cls, owner, qual_name, *args, **kw):
+
+    # In certain edge cases, repr can generate RecursionError; 
+    # c.f., ExceptionThrowingParser
+    zargs = []
+    for a in args:
+      if type(a) is owner:
+        zargs.append(object.__repr__(a))
+      else:
+        zargs.append(repr(a))
+    args_s = ", ".join(zargs)
+
+    zkw = {}
+    for k,v in kw.items():
+      if type(v) is owner:
+        zkw[k] = object.__repr__(v)
+      else:
+        zkw[k] = repr(v)
+    kw_s = ", ".join([f"{k}={v}" for k,v in zkw.items()])
+
     s = ",".join(
       [item for item in (args_s, kw_s) if item != ""]
     )
@@ -37,40 +65,40 @@ class TraceClassDecorator(object):
     _printf_debug(msg, prefix='') 
 
   @classmethod
-  def _wrap_call(cls, fn, *args, **kw):
+  def _wrap_call(cls, owner, fn, *args, **kw):
     cls._indent = cls._indent + 2
-    cls._debug_call(fn.__qualname__, *args, **kw)
+    cls._debug_call(owner, fn.__qualname__, *args, **kw)
+
     try:
       val = fn(*args, **kw)
     finally:
       cls._indent = cls._indent - 2
     return val
 
-  def wrapper(self, fn):
+  def wrapper(self, cls, fn):
     @functools.wraps(fn)
     def wfn(*args, **kw):
-      return TraceClassDecorator._wrap_call(fn, *args, **kw)
+      return TraceClassDecorator._wrap_call(cls, fn, *args, **kw)
     
-    setattr(wfn, ".__scaffold_wrapped", None)
+    setattr(wfn, "__scaffold_wrapped", None)
     return wfn
 
   def wrap(self, cls, attr_name):
     attr = cls.__dict__.get(attr_name)
+
     if hasattr(attr, '__scaffold_wrapped'):
       return
 
-    # qual_name = ".".join([cls.__name__, attr_name])
-
     if isinstance(attr, types.FunctionType):
-      wrapped = self.wrapper(attr)
+      wrapped = self.wrapper(cls, attr)
       setattr(cls, attr_name, wrapped)
 
     elif isinstance(attr, classmethod):
-      wrapped = self.wrapper(attr.__func__)
+      wrapped = self.wrapper(cls, attr.__func__)
       setattr(cls, attr_name, classmethod(wrapped))
 
     elif isinstance(attr, staticmethod):
-      wrapped = self.wrapper(attr.__func__)
+      wrapped = self.wrapper(cls, attr.__func__)
       setattr(cls, attr_name, staticmethod(wrapped))
 
 
@@ -79,7 +107,7 @@ class TraceClassDecorator(object):
     for K in class_set:
       attrs = K.__dict__.keys()
 
-      for attr_name in attrs:
+      for attr_name in attrs:        
         if self._include:
           if attr_name not in self._include:
             continue
@@ -102,7 +130,7 @@ if __name__ == '__main__':
       """
       Some documentation for A.__init__
       """
-      self.ivar = None
+      pass
 
     def foo(self, v : int):
       """
@@ -120,6 +148,12 @@ if __name__ == '__main__':
 
   @TraceClassDecorator(mro = True)
   class B(A):
+    def __init__(self):
+      """
+      Some documentation for B.__init__
+      """
+      super().__init__()
+
     def foo(self, v : int):
       """
       Some documentation for B.foo
@@ -139,3 +173,4 @@ if __name__ == '__main__':
   b.foo(0)
 
   B.baz()
+
