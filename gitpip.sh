3794bc6eb990b2f1f083094f2c67264b25d71625
Dustin Lennon <dustin.lennon@gmail.com>
2025-04-01 08:33:43 -0700

    Added version to gitpip.sh

--- gitpip.sh
+++ gitpip.sh
@@ -29,7 +29,7 @@ if [ ! -f Pipfile ]; then
 fi
 
 # pipenv install file://${HOME}/Workspace/Sandbox/scaffold-params#egg=scaffold
-pipenv install git+https://github.com/dustinlennon/scaffold-params#egg=scaffold
+pipenv install git+https://github.com/dustinlennon/scaffold-params@v0.0.3#egg=scaffold
 
 # make samples easily available
 scaffold_path=$(pipenv run python -c "import scaffold; print(scaffold.__path__[0])")
